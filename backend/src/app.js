fbc1881cc67ad3c42c2804f3c0f21e50270d4e04
ASHOKJAAT12 <ajat39963@gmail.com>
2026-05-21 23:38:07 +0530

    Make a asyncHandler.js in utils file to handle error and without writing try-catch anywhere in backend project

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import cros from 'cros';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cros({
-    origin: "http://localhost:3000/",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 app.use(express.urlencoded({
