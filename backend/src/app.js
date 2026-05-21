4200fd114e8fb97bb5bc6bc817de54fd8a39fabb
ASHOKJAAT12 <ajat39963@gmail.com>
2026-05-21 22:14:58 +0530

    practice of change in the app.js in backend project

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,22 @@
 import express from 'express';
-import cors from 'cros';
-import cookieparser from 'cookie-parser';
+import cros from 'cros';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: 'http://localhost:3000',
-        credentials: true
-    }
-))
-
-app.use(cookieparser());
-app.use(express.static("public"));
+app.use(cros({
+    origin: "http://localhost:3000/",
+    credentials: true
+}));
+app.use(express.urlencoded({
+    limit: "16kb",
+    extended: true
+}));
 app.use(express.json({
     limit: "16kb"
 }));
-app.use(express.urlencoded({
-    extended: true,
-    limit: "16kb"
-}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 
 
