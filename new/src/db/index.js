fe37983e39318edaaafe58f96925de3d0e367449
ASHOKJAAT12 <ajat39963@gmail.com>
2026-05-26 22:59:09 +0530

    practice in new project of mongodb connection

--- new/src/db/index.js
+++ new/src/db/index.js
@@ -1,11 +1,15 @@
-import mongoose from "mongoose";
-async function connectDB() {
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectionDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/mydb");
-        console.log(`Connected to MongoDB at ${conn.connection.host}`);
+        const mongodbconection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        //mongodb connection successfull
+        console.log(`mongodb connection ${mongodbconection.Connection.prototype}`);
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
+        console.log(`mongodb connection faild!`);
         process.exit(1);
     }
 }
-export default connectDB;
+
+export default connectionDB;
