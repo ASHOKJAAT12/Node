26417036aaca23e1064fb7631bfeec86984cfda8
ASHOKJAAT12 <ajat39963@gmail.com>
2026-05-27 13:28:29 +0530

    register work panding in backend project

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,26 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
-
+import { ApiError } from '../utils/ApiError.js'
 const registerUser = asyncHandler(async (req, res)=> {
-    res.status(200).json({
-        message: "Done"
-    })
+    //take input from front end
+    //validation not empty
+    //check the username and email is already exist
+    //check for image , check for avatar
+    //uplaod on cloudinary
+    //create user object
+    //remove password and refersetoken field from response
+    //check user creation 
+    //return response
+
+    const {userName, fullName, email, coverImage, avatar, password} = req.body;
+
+    if (
+        [userName, fullName, email, coverImage, avatar, password].some(
+            (field)=> field?.trim() === ""
+        )
+    ) {
+        throw new ApiError(400,"All Feilds re Required!");
+    }
+    
 })
 
 export { registerUser } ;
