666bebbd05e7a2bba682c7e27645fe233f4048eb
ASHOKJAAT12 <ajat39963@gmail.com>
2026-06-04 13:05:26 +0530

    authorization middleware add

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { User } from '../models/user.models.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import jwt from 'jsonwebtoken';
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -145,4 +146,8 @@ const loginUser = asyncHandler( async (req, res) => {
         },"user successfully login.")
     )
 })
+
+
+
+
 export { registerUser, loginUser } ;
