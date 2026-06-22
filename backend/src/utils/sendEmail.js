a0359b2ce5c34df16a8359727aed57383e8d7af5
ASHOKJAAT12 <ajat39963@gmail.com>
2026-06-22 20:04:40 +0530

    nodemailer dependencies add

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,9 @@
+import nodemailer from 'nodemailer';
+import { asyncHandler } from './asyncHandler';
+
+const sendEmail = asyncHandler ( async (to , subject, html) => {
+
+})
+
+
+export { sendEmail };
