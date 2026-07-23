b97875d75f78213b7be124379904f9dae17f7b48
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-23 16:33:28 +0530

    email send fuctionalty add

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,14 +1,45 @@
 import nodemailer from 'nodemailer';
 import Mailgen from 'mailgen';
 import { useActionState } from 'react';
+import { text } from 'express';
+import Mail from 'nodemailer/lib/mailer';
 
 const sendMail = (options)=> {
-    const MailGenerator = new Mailgen(
+    const MailGenerator = new Mailgen({
         theme: "default",
         product: {
-            name
+            name: "Task Manager",
+            link: "https://taskmanager.com"
         }
-    )
+    })
+
+    const emailTextual = MailGenerator.generatePlaintext(options.MailgenContent);
+
+    const emailHtml = MailGenerator.generate(options.MailgenContent);
+
+    const transport = nodemailer.createTransport({
+        host: process.env.NODEMAILER_SMTP_HOST,
+        port: process.env.NODEMAILER_SMTP_PORT,
+        auth: {
+            user: process.env.NODEMAILER_SMTP_USER,
+            pass: process.env.NODEMAILER_SMTP_PASS
+        }
+    });
+
+    const mail = {
+        from: "taskmanager@gamil.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHtml
+    };
+
+    try {
+        await transport.sendMail(mail);
+    } catch (error) {
+        console.error("Email service failed siliently. Make sure that you have provided your MAILTRAP creadentials in the .env file");
+        console.error("Error: ", error);
+    }
 }
 const emailVerificationMailgenContent = (username , verificationUrl) => {
     return {
